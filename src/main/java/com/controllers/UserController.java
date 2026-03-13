e3b7cf61b272df2c0660eb45957dac6b1bafddfc
GustavoLenni <gustavogithub1@gmail.com>
2026-03-13 18:56:30 -0300

    Fixed any methods and implementing the method of GetByID

--- src/main/java/com/controllers/UserController.java
+++ src/main/java/com/controllers/UserController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/users")
@@ -18,12 +19,24 @@ public class UserController {
     @Autowired
     UserRepository repository;
 
+    // Get Find all
     @GetMapping
     public ResponseEntity getall(){
         List<User> listUsers = repository.findAll();
         return ResponseEntity.status(HttpStatus.OK).body(listUsers);
     }
 
+    // Get by ID
+    @GetMapping("/{id}")
+    public ResponseEntity getByID(@PathVariable Integer id){
+        var listUserbyID = repository.findById(id);
+        if(listUserbyID.isEmpty()){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Has no user by this ID");
+        }
+        return ResponseEntity.status(HttpStatus.FOUND).body(listUserbyID);
+    }
+
+    // Post
     @PostMapping
     public ResponseEntity saveUser(@RequestBody UserDto dto){
         var user = new User();
@@ -31,12 +44,14 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(user));
     }
 
+    // Delete
     @DeleteMapping("/{id}")
     public ResponseEntity deleteUser(@PathVariable Integer id){
         repository.deleteById(id);
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.status(HttpStatus.OK).body("this user was deleted");
     }
 
+    // Update
     @PutMapping("/{id}")
     public ResponseEntity updateUser(@PathVariable Integer id,@RequestBody UserDto dto){
         var user = repository.findById(id).orElse(null);
